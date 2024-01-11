ebd00fd113f951248e23d2f5d122b52f6e84537f
KristinaMogorovic <kristina.mogorovic1@gmail.com>
2024-01-11 14:17:58 +0100

    file za prekidanje sesije

--- php/obrada_log_in.php
+++ php/obrada_log_in.php
@@ -1,8 +1,13 @@
 <?php
+
+    $server = "student.veleri.hr";
+    $database = "kmogorovi";
+    $username = "kmogorovi";
+    $password ="6929";
     
-    $iskaznica =$_GET['iskaznica'];
+    //$iskaznica =$_GET['iskaznica'];
     // $pin=$_GET['pin']; // pusti zakomentirano -->pin je stavljen pod type="password"
 
-    echo "Pozdrav $iskaznica. Prijavljeni ste. <br>";
+    //echo "Pozdrav $iskaznica. Prijavljeni ste. <br>";
     
 ?>
